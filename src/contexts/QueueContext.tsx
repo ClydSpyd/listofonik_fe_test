e94a23af465fa6a490baca470403086050c02b3c
Dave Clydesdae <daveclydesdale@gmail.com>
2023-03-27 20:58:23 +0200

    queue context added

--- src/contexts/QueueContext.tsx
+++ src/contexts/QueueContext.tsx
@@ -1,33 +1,31 @@
-import { createContext, useState, useContext } from 'react'
+import { createContext, useState, useContext } from "react";
 
 interface QueueContextObject {
-  queue: QueueData | null,
-  setQueue: React.Dispatch<React.SetStateAction<QueueData | null>>
+  queue: QueueData | null;
+  setQueue: React.Dispatch<React.SetStateAction<QueueData | null>>;
 }
 
 const defaultContext: QueueContextObject = {
   queue: [],
   setQueue: (): void => {},
+};
+
+const QueueContext = createContext<QueueContextObject>(defaultContext);
+
+interface Props {
+  children: React.ReactNode;
 }
 
-const QueueContext = createContext<QueueContextObject>(
-  defaultContext)
-
-  interface Props {
-    children: React.ReactNode
-  }
-
-export const QueueContextProvider = ({children} : Props) => {
-  const [ queue, setQueue ] = useState<QueueData|null>(null)
+export const QueueContextProvider = ({ children }: Props) => {
+  const [queue, setQueue] = useState<QueueData | null>(null);
 
   return (
-    <QueueContext.Provider value={{queue, setQueue}}>
+    <QueueContext.Provider value={{ queue, setQueue }}>
       {children}
     </QueueContext.Provider>
-  )
-
-}
+  );
+};
 
 export const useQueue = (): QueueContextObject => {
-  return useContext(QueueContext)
-} 
+  return useContext(QueueContext);
+};
